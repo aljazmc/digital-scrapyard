e3b6c27d6b526018ee4a36ba999a9547561773fd
aljazmc <mlinaric.aljaz@gmail.com>
2025-01-10 16:51:39 +0100

    chore: remove react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,16 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter, Routes, Route } from "react-router";
+
+function App() {
+  return <h1>App is here!</h1>;
+}
 
 import "./index.css";
-
-import App from "./App";
-import Buttons from "./piles/Buttons";
-import Navigations from "./piles/Navigations";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/buttons" element={<Buttons />} />
-        <Route path="/navigations" element={<Navigations />} />
-      </Routes>
-    </HashRouter>
+    <App />
   </StrictMode>,
 );
